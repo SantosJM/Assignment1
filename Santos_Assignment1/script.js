12a5249ba0a536f028116953cc06eaefe1d54cab
SantosJM <jmichellesantos@gmail.com>
2015-09-17 13:59:29 -0700

    Assignment1

---
+++ Santos_Assignment1/script.js
@@ -0,0 +1,180 @@
+// ====== 1.======
+// someMonth
+// theMonth
+// currentMonth
+// summerMonths
+// myLibrary
+
+
+// ====== 2.======
+//1.0
+//"I ain't got time for dat!"
+//true
+//null
+
+
+// ====== 3.======
+// var rolledTacos = 6 - 1;
+// var barf = rolledTacos * 4;
+
+
+// ====== 4.======
+// var firstName;
+// var lastName;
+// var address;
+// var city;
+// var state;
+// var zipCode;
+// var yourAge;
+// var referralSource;
+// var mayWeContactYou;
+
+
+// ====== 5.======
+//METHOD 1:
+//var firstName;
+//var lastName;
+//var address;
+//var city;
+//var state;
+//var zipCode;
+//var yourAge;
+//var referralSource;
+//var mayWeContactYou;
+//firstName = "Jan Michelle";
+//lastName= "Santos";
+//address = "555 nowhere";
+//city = "San Diego";
+//state = "California";
+//zipCode = "91914";
+//yourAge = "38";
+//referralSource = "FEWD";
+//mayWeContactYou = true;
+
+//METHOD 2:
+//var firstName = "Jan Michelle";
+//var lastName = "Santos";
+//var address = "555 nowhere";
+//var city = "San Diego";
+//var state = "California";
+//var zipCode = "91914";
+//var yourAge = "38";
+//var referralSource = "FEWD";
+//var mayWeContactYou = true;
+
+//METHOD 3:
+//var firstName = "Jan Michelle", lastName = "Santos", address = "555 nowhere", city = "San Diego", state = "California", zipCode = "91914", yourAge = "38", referralSource = "FEWD", mayWeContactYou = true;
+
+
+// ====== 6. ======
+// var x = 1 + iphone;
+// console.log(x);
+
+// var y =
+// console.log(y);
+
+// var z =
+// console.log(z);
+
+
+// ====== 7. ======
+
+
+// ====== 8. ======
+// It is not valid.
+
+// I WOULD FIX IT LIKE THIS:
+//var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+//console.log(someString);
+
+
+// ====== 9. ======
+// var y = null;
+//    console.log(y);
+//var x;
+//    console.log(x);
+
+
+// ====== 10. ======
+// console.log(typeof "john");
+// console.log(typeof 5.02);
+// console.log(typeof false);
+// console.log(typeof {name:'jan', age:38});
+// console.log(typeof myName);
+
+
+// ====== 11. ======
+// alert("Hello" + " Jan Michelle Santos" + ", welcome to the Javascript class!");
+
+
+// ====== 12. ======
+// var name="Jan Michelle Santos";
+// alert(name);
+
+
+// ====== 13. ======
+// var course="Javascript";
+// var name="Jan Michelle Santos";
+// alert("Hello " + name + ", welcome to " + course);
+
+
+// ====== 14. ======
+// var course="Javascript class!";
+// var name = "Jan Michelle Muthafuckin' Santos";
+// alert("Hello " + name + "." + "\nWelcome to " + course);
+
+
+// ====== 15. ======
+// var course="Javascript class!";
+// var name = prompt("What's yo' muthafuckin' name?");
+// alert("Hello " + name + "." + "\nWelcome to " + course);
+
+
+// ====== 16. ======
+// var course=prompt("What class are you taking?");
+// var name = prompt("What's yo' muthafuckin' name?");
+// alert("Hello " + name + "." + "\nWelcome to " + course);
+
+
+// ====== 17. ======
+// var x = 10;
+// var y = 20;
+// console.log(x+y);
+
+
+// ====== 18. ======
+// var x = 20;
+// x += 20;
+// console.log(x);
+
+
+// ====== 19. ======
+// var x = 20;
+// x *= 5;
+// console.log(x);
+
+
+// ====== 20. ======
+// var x = (20%3);
+// x /= 1;
+// alert(x);
+
+
+// ====== 21. ======
+// var x = 5;
+// var y = 10;
+// console.log(x <= y !=false);
+
+
+// ====== 22. ======
+// var x = 1;
+// var y = 100;
+// alert(x == y !=false);
+
+
+// ====== 23. ======
+// var widget = new Object();
+// console.log(typeof "widget");
+
+//24.
+//25.
